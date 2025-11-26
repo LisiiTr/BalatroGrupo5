353918dc4e3a60ca8c10c893b3988db53920a2bb
Abril <adebiasi@uade.edu.ar>
2025-11-26 20:53:52 -0300

    Pruebas unitarias de archivo combinaciones.py

--- test_combinaciones.py
+++ test_combinaciones.py
@@ -100,4 +100,38 @@ def test_trio():
 	resultado=combinaciones.trio([9,9,5,2,3])
 	assert resultado == False
 
+def testColor():
+    assert combinaciones.color(['♥','♥','♥','♥','♥']) == True # True: 5 corazones
+    assert combinaciones.color(['♥','♥','♣','♥','♥']) == False  # False: mezcla de palos
+    assert combinaciones.color(['♥','♥','♥','♥']) == False # False: longitud distinta de 5
 
+def test_dividirPaloValores():
+	# Caso de prueba: una mano de 5 cartas con distintos valores y palos (con un valor repetido).
+    cartas = [
+        {"valor": 1,  "palo": "♠"},
+        {"valor": 10, "palo": "♥"},
+        {"valor": 13, "palo": "♦"},
+        {"valor": 7,  "palo": "♣"},
+        {"valor": 5,  "palo": "♣"},
+    ]
+	# Asegura que la funcion separa correctamente una lista de cartas en dos listas paralelas de valor y palo, sin perder el orden 
+    valores, palos = combinaciones.dividirPaloValores(cartas)
+    assert valores == [1,10,13,7,5]
+    assert palos   == ["♠","♥","♦","♣","♣"]
+
+def test_analizarCombinacionesJokers_flags():
+    # Caso de prueba: escalera de color (5 consecutivas mismo palo)
+    cartas_test = [
+        {"valor": 5, "palo": "♥"},
+        {"valor": 6, "palo": "♥"},
+        {"valor": 7, "palo": "♥"},
+        {"valor": 8, "palo": "♥"},
+        {"valor": 9, "palo": "♥"},
+    ]
+	# Debe detectar escalera de color (ambas banderas verdaderas y la combinada también)
+    assert combinaciones.analizarCombinacionesJokers(cartas_test)["escalera_corrida"] == True # color + escalera
+    assert combinaciones.analizarCombinacionesJokers(cartas_test)["color"] == True # mismo palo en 5 cartas
+    assert combinaciones.analizarCombinacionesJokers(cartas_test)["escalera"] == True # 5 valores consecutivos
+	# Asegura que no marque otras combinaciones que no sea "escalera"
+    assert combinaciones.analizarCombinacionesJokers(cartas_test)["poker"] == False # no hay 4 iguales
+    assert combinaciones.analizarCombinacionesJokers(cartas_test)["full_house"] == False # no hay 3+2
